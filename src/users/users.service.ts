2e486be632418544993eeab8be19b5ed2e53387b
Wesley Monteiro <wmonteiro873@gmail.com>
2025-08-13 16:58:24 -0300

    :test_tube: test: userController unit tests

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException, Post } from "@nestjs/common";
 import { Prisma } from "@prisma/client";
-import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaService } from "../prisma/prisma.service";
 import { User } from "@prisma/client";
 @Injectable()
 export class UserService {
